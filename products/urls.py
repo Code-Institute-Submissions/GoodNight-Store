ba02f24d554c01bc334f99b509309f5aec68500c
dejvoss <deosiecki@gmail.com>
2020-11-24 16:41:44 +0100

    Add the products view and product details page

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views         #import views from home path
 
 
 urlpatterns = [
-    path('', views.all_products, name='products'),   
+    path('', views.all_products, name='products'),
+    path('<product_id>', views.product_details, name='product_details'),   
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+from django.conf import settings
+
+
+MEDIA_URL = settings.MEDIA_URL
 
 # Create your views here.
 def all_products(request):
@@ -12,3 +16,14 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+def product_details(request, product_id):
+    """ A view for product details - shows the details of selected product """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product
+    }
+
+    return render(request, 'products/product_details.html', context)
