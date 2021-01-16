4f0a7acbf2969d1b939c6b7acf211c15b47feb1f
dejvoss <deosiecki@gmail.com>
2021-01-16 13:21:29 +0100

    Added page and funcionality to editing the product

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,6 @@ from . import views         #import views from home path
 urlpatterns = [
     path('', views.all_products, name='products'),
     path('<int:product_id>/', views.product_details, name='product_details'),
-    path('add/', views.add_product, name='add_product')
+    path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product')
 ]
