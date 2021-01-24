a07c7fd9673015dcb81a64c941a1ddca1f49b850
dejvoss <deosiecki@gmail.com>
2021-01-24 11:41:15 +0100

    Added post model in to the blog app

--- gnblog/models.py
+++ gnblog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = (
+    (0, 'Draft'),
+    (1, 'Publish')
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=256, unique=True, null=False, blank=False)
+    subtitle = models.CharField(max_length=512, null=True, blank=True)
+    body = models.TextField()
+    postUrl = models.SlugField(max_length=256, unique=True)
+    author = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default="anonymous author")
+    created = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created']
+
+
+    def __str__(self):
+        return self.title
+
