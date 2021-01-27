a3432096de88a9ae6b1307a0731a02bab1efc43f
dejvoss <deosiecki@gmail.com>
2021-01-27 10:53:29 +0100

    changed the url for background images

--- goodnight_store/settings.py
+++ goodnight_store/settings.py
@@ -14,16 +14,9 @@ from pathlib import Path
 import os
 import dj_database_url
 
-# Import environ extension and environ file
-# import environ
-# env = environ.Env()
-
-# Read environ file
-# environ.Env.read_env()
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
